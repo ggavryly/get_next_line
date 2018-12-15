89f24974a18ae1e65e4fbb4a5b82926e2e2a045b
ggavryly <torgo121@gmail.com>
2018-12-15 18:39:42 +0200

    get_next_line

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ggavryly <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/03 14:38:50 by ggavryly          #+#    #+#             */
+/*   Updated: 2018/11/03 14:49:02 by ggavryly         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 32
+# include "libft/libft.h"
+
+int		get_next_line(const int fd, char **line);
+
+#endif
